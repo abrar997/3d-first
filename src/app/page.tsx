1370cfe004d54a6162e41197c8bb9d8c4629c19f
abrar997 <abraralrawi997.com>
2024-08-07 17:29:02 +0300

    fix loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 "use client";
-import dynamic from "next/dynamic";
 import { useState, useEffect } from "react";
+import dynamic from "next/dynamic";
 import Loading from "./loading";
 
 const Canvas = dynamic(() => import("../components/Canvas"), { ssr: false });
 
 function Home() {
-  const [loading, setLoading] = useState(false);
-  useEffect(() => setLoading(true));
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
   return <div>{loading ? <Loading /> : <Canvas />}</div>;
 }
 
